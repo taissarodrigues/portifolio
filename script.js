dd2b3b8b341bb519f6b9d1b85eeedd4ec3b0ae7c
taissavitoria <taissavrp@gmail.com>
2025-08-13 20:49:55 -0300

    mudancas

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-function toggleMenu() {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
+<script>
+  function toggleMenu(){
+    const wrap = document.querySelector('#hamburguer-nav');
+    if(!wrap) return;
+    const menu = wrap.querySelector('.menu-links');
+    const icon = wrap.querySelector('.hamburguer-icon');
+    menu.classList.toggle('open');
+    icon.classList.toggle('open');
   }
-
+</script>
